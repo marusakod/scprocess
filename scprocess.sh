d4e57694a88f37ab32e2a2583869f8a7f0de8ba6
Marusa Koderman <marusa.koderman@gmail.com>
2024-07-26 10:31:44 +0200

    added core snakemake files

---
+++ scprocess.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "running snakemake with the following inputs:"
+echo "  snakefile:          $1"
+echo "  config file:        $2"
+echo "  project directory:  $3"
+echo "  rule to run:        $4"
+echo "  extra arguments:    $5"
+echo
+
+snakefile=$1
+configfile=$2
+proj_dir=$3
+rule=$4
+extraargs=$5
+
+# get directory of this file, change to it
+SCPROCESS_DIR="$(dirname "$(readlink -f "$0")")"
+cd $SCPROCESS_DIR
+
+echo "starting snakemake rule" $rule
+
+SM_ARGS=" "
+
+if [ "$extraargs" != 0 ]
+  then
+   SM_ARGS+="$extraargs"
+fi
+
+# run snakemake
+
+echo snakemake ${SM_ARGS} \
+  --snakefile $snakefile \
+  --configfile $configfile \
+  --rerun-incomplete \
+  --verbose \
+  --use-conda \
+
+ snakemake ${SM_ARGS} \
+  --snakefile $snakefile \
+  --configfile $configfile \
+  --rerun-incomplete \
+  --verbose \
+  --use-conda \
+
