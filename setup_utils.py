011fe36f6f3c6a222c3293e9621c070e6a185fcf
Marusa Koderman <marusa.koderman@gmail.com>
2024-08-25 20:46:04 +0200

    changed config format

--- setup_utils.py
+++ setup_utils.py
@@ -32,30 +32,25 @@ def get_setup_parameters(config):
 
   if('tenx' in config['genome']):
     
-    names = config['genome']['tenx']['name']
-    if isinstance(names, str):
-      names = [names]
+    names = [entry['name'] for entry in config['genome']['tenx']]
+    decoys = [entry.get('decoys', None) for entry in config['genome']['tenx']]
 
+    
     # check that all genome names are unique
     assert len(names) == len(set(names)), "Duplicated genome names are not allowed!"
     
-    assert np.isin(names, allowed_names), "unrecognized 10x genome name"
+    assert all(name in allowed_names for name in names), "unrecognized 10x genome name"
      
     # set defaults
     fasta_fs = ['None'] * len(names)
     gtf_fs = ['None'] * len(names)
     mito_strs = ['None'] * len(names)
-    decoys = [False] * len(names)
     
-    # change defaults for decoys if available
-    if 'decoys' in config['genome']['tenx']:
-      decoys = config['genome']['tenx']['decoys']
-      if isinstance(decoys, bool):
-        decoys = [decoys]
-      
-      #check that lenght of decoys is the same as names
-      params_ls_len = {len(lst) for lst in [names, decoys]}
-      assert len(params_ls_len) == 1, "10x genome parameters don't have the same lengths"
+    decoys = [True if d is None else d for d in decoys]
+    
+    #check that lenght of decoys is the same as names
+    params_ls_len = {len(lst) for lst in [names, decoys]}
+    assert len(params_ls_len) == 1, "10x genome parameters don't have the same lengths"
         
     # update params lists
     all_genome_names.extend(names)
@@ -67,13 +62,21 @@ def get_setup_parameters(config):
     # check custom entries  
   if 'custom' in config['genome']:
       # check if all required entries are specified
-    req = ['name', 'fasta', 'gtf', 'mito_str']
-    for r in req:
-      assert r in config['genome']['custom'], f"missing {r} in custom genome specification"
+
+    has_name_key      = ['name' in entry for entry in config['genome']['custom']]
+    assert all(has_name_key), \
+     "missing 'name' in custom genome specification"
+    has_fasta_key     = ['fasta' in entry for entry in config['genome']['custom']]
+    assert all(has_fasta_key), \
+     "missing 'fasta' in custom genome specification"
+    has_gtf_key       = ['gtf' in entry for entry in config['genome']['custom']]
+    assert all(has_gtf_key), \
+     "missing 'gtf' in custom genome specification"
+    has_mito_str_key  = ['mito_str' in entry for entry in config['genome']['custom']]
+    assert all(has_mito_str_key), \
+      "missing 'mito_str' in custom genome specification"
         
-    cust_names = config['genome']['custom']['name']
-    if isinstance(cust_names, str):
-      cust_names =[cust_names]
+    cust_names = [entry['name'] for entry in config['genome']['custom']]
 
     # check that all genome names are unique
     assert len(cust_names) == len(set(cust_names)), "Duplicated genome names are not allowed!"
@@ -82,38 +85,27 @@ def get_setup_parameters(config):
     assert len(set(cust_names) & set(allowed_names)) == 0, \
       "Some names for custom genomes overlap names for 10x genomes; please define other names for custom genomes"
 
-    cust_fastas = config['genome']['custom']['fasta']
-    if isinstance(cust_fastas, str):
-      cust_fastas =[cust_fastas]
-        
-    cust_gtfs = config['genome']['custom']['gtf']
-    if isinstance(cust_gtfs, str):
-      cust_gtfs =[cust_gtfs]
-        
-    cust_mito_strs = config['genome']['custom']['mito_str']
-    if isinstance(cust_mito_strs, str):
-      cust_mito_strs =[cust_mito_strs]
+    cust_fastas =  [entry['fasta'] for entry in config['genome']['custom']]
+    cust_gtfs =  [entry['gtf'] for entry in config['genome']['custom']]
+    cust_mito_strs = [entry['mito_str'] for entry in config['genome']['custom']]
       
-      # get defaults for decoys
-    cust_decoys = [False] * len(cust_names)
-      
-    if 'decoys' in config['genome']['custom']:
-      cust_decoys = config['genome']['custom']['decoys']
-      if isinstance(cust_decoys, bool):
-        cust_decoys = [cust_decoys]
+    # get decoys
+    cust_decoys = [entry.get('decoys', None) for entry in config['genome']['custom']]
+    cust_decoys = [True if d is None else d for d in cust_decoys]
+    
         
-      # check if all entries have the same length
+    # check if all entries have the same length
     cust_params_ls_len = {len(lst) for lst in [cust_names, cust_fastas, cust_gtfs, cust_mito_strs, cust_decoys]}
     assert len(cust_params_ls_len) == 1, "custom parameters don't have the same lengths"
     
       # check if fasta and gtf files exist
-    for fa in cust_fastas:
-      assert os.path.isfile(fa), \
-        f"file {fa} specified in configfile doesn't exist"
+    # for fa in cust_fastas:
+    #   assert os.path.isfile(fa), \
+    #     f"file {fa} specified in configfile doesn't exist"
           
-    for gtf in cust_gtfs:
-      assert os.path.isfile(gtf), \
-        f"file {gtf} specified in configfile doesn't exist"
+    # for gtf in cust_gtfs:
+    #   assert os.path.isfile(gtf), \
+    #     f"file {gtf} specified in configfile doesn't exist"
         
 
     # update params lists
@@ -136,4 +128,3 @@ def get_setup_parameters(config):
 
   # return lists with all params
   return genome_names_str, fasta_fs_str, gtf_fs_str, mito_one_str, decoys_str
-  #return all_genome_names, all_fasta_fs, all_gtf_fs, all_mito_str, all_dcoys
