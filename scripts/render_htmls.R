8e312c73d5c1278fedb5cc5f3b6843b6b515d494
Will Macnair <macnairw@rkanc001is01.eth.rsiec.kau.science.roche.com>
2025-10-15 16:21:57 +0200

    various tweaks to plots

--- scripts/render_htmls.R
+++ scripts/render_htmls.R
@@ -132,7 +132,7 @@ get_sub_ls <- function(rule = c('af', 'multiplexing', 'ambient', 'qc', 'hvg', 'i
 
   } else if (sel_rule == 'integration') {
     req_names = c('YOUR_NAME', 'AFFILIATION', 'SHORT_TAG', 'PROJ_DIR', 
-      'DATE_STAMP', 'threads', 'qc_dt_f', 'integration_f', 'int_reduction', 'demux_type', 
+      'DATE_STAMP', 'threads', 'qc_dt_f', 'integration_f', 'int_embedding', 'demux_type', 
       'int_res_ls_str', 'int_dbl_cl_prop')
 
     assert_that(all(req_names %in% add_args_names))
